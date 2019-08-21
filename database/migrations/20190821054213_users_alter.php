0c9723167156d29869203b924b8bf98748ea43d4
kkmym <kentarokamiyama@gmail.com>
2019-08-21 21:44:10 +0900

    Phinxを動かしてみる

---
+++ database/migrations/20190821054213_users_alter.php
@@ -0,0 +1,39 @@
+<?php
+
+
+use Phinx\Migration\AbstractMigration;
+
+class UsersAlter extends AbstractMigration
+{
+    /**
+     * Change Method.
+     *
+     * Write your reversible migrations using this method.
+     *
+     * More information on writing migrations is available here:
+     * http://docs.phinx.org/en/latest/migrations.html#the-abstractmigration-class
+     *
+     * The following commands can be used in this method and Phinx will
+     * automatically reverse them when rolling back:
+     *
+     *    createTable
+     *    renameTable
+     *    addColumn
+     *    renameColumn
+     *    addIndex
+     *    addForeignKey
+     *
+     * Remember to call "create()" or "update()" and NOT "save()" when working
+     * with the Table class.
+     */
+    public function change()
+    {
+        $table = $this->table('users', ['id' => false, 'primary_key' => ['user_id'], 'collation' => 'utf8mb4_general_ci']);
+        $table->addColumn('login_id', 'string', ['encoding' => 'utf8mb4'])
+            ->addColumn('hashed_pw', 'string', ['encoding' => 'utf8mb4'])
+            ->addColumn('user_name', 'string', ['encoding' => 'utf8mb4'])
+            ->addColumn('created_at', 'timestamp', ['default' => 'CURRENT_TIMESTAMP'])
+            ->addColumn('updated_at', 'timestamp', ['default' => 'CURRENT_TIMESTAMP', 'update' => 'CURRENT_TIMESTAMP'])
+            ->save();
+    }
+}
